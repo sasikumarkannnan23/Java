3e9399e513277ae9bc210ff824e86926e47b46ed
sasikumarkannnan23 <134365742+sasikumarkannnan23@users.noreply.github.com>
2023-05-23 12:42:23 +0530

    Add files via upload

---
+++ FirstAssignment1.java
@@ -0,0 +1,36 @@
+package com.sk.assignment1;
+
+public class FirstAssignment1 {
+static String s1="India";
+static String s2="England";
+
+public void Asia()
+{
+	System.out.println(s1);
+}
+public void Europe()
+{
+	System.out.println(s2);
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirstAssignment1 Inst=new FirstAssignment1();
+		Inst.Asia();
+		Inst.Europe();
+		Secound S1=new Secound();
+		S1.Europes();
+	}
+	
+
+}
+  class Secound{
+	static String s3="Italy";
+	public void Europes()
+	{
+		System.out.println(s3);
+		
+	}
+	
+}
